6a8ebea4fd733ad2cc7e96d6f155d07cc6be03de
bmigo <berniecmigo@gmail.com>
2023-02-04 11:24:16 -0500

    User model

--- models/User.js
+++ models/User.js
@@ -2,17 +2,18 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
     {
         username: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
         },
         email: {
-
+            type: String,
+            unique: true,
+            required: true,
         },
-        thoughts: {
-
-        },
-        friends: {
-
-        }
+        thoughts: [thoughtSchema],
+        friends: [userSchema]
     }
 );
 
