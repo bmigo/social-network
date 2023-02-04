7bf77cb5b5274c54cf2bbf15a72465ee78cf7e77
bmigo <berniecmigo@gmail.com>
2023-02-04 11:16:28 -0500

    models

---
+++ models/Thought.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+
+        },
+        createdAt: {
+
+        },
+        username: {
+
+        },
+        reactions: {
+
+        }
+    }
+);
+
+const Thought = model ('thought', thoughtSchema);
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+const userSchema = new Schema(
+    {
+        username: {
+
+        },
+        email: {
+
+        },
+        thoughts: {
+
+        },
+        friends: {
+
+        }
+    }
+);
+
+const User = model('user', userSchema);
+module.exports = User;
